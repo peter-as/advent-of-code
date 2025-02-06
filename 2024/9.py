7a42fa1b66a946f94a32e34e42317fc2b0b63780
Peter Aszalós <p.aszalos-1@student.tudelft.nl>
2025-02-06 22:52:47 +0000

    updated and sped up 2024/9

--- 2024/9.py
+++ 2024/9.py
@@ -1,13 +1,16 @@
 def read_file():
-    f = open("9.txt","r")
+    # I read the file and 
+    f = open("2024/9.txt","r")
+    # index keeps track of the number to use, step is if we have empty space or numbers, last keeps track how many characters -1 we have
+    # l contains a character for every spot, list_of_batches keeps track of how many of what characters we have in a batch
     l = []
     index = 0
     step = 0
     last = -1
-
     list_of_batches = []
     for i in f.read():
         i = int(i)
+        # If step is 0 we have numbers
         if step == 0:
             for k in range(i):
                 l.append(index)
@@ -15,6 +18,7 @@ def read_file():
             last += i
             index += 1
             step = 1
+        # If step is 1 we have empty space
         else:
             for k in range(i):
                 l.append(".")
@@ -24,6 +28,7 @@ def read_file():
     return l, last, list_of_batches
 
 def calculate_checksum(line):
+    # We calculate the checksum on the line by looping over it and adding the number * index to a sum
     checksum = 0
     for i in range(len(line)):
         if not line[i] == ".":
@@ -33,13 +38,17 @@ def calculate_checksum(line):
 def move_files(line, last):
     num = 0
     last_index = 0
+    # We find where the last number is in the list
     for i in range(len(line)-1,0,-1):
         if not line[i] == ".":
             num = int(line[i])
             last_index = i
             break
     first_index = 0
+    # We progress backwards through the list, and try to move things to the beginning
+    # We do this until our back index reaches the number of numbers we have, since we are done after that
     while last_index > last and first_index < last + 1:
+        # If we have an available spot, we progress backwards until we get to a number, and move it there
         if line[first_index] == ".":
             while line[last_index] == ".":
                 last_index -= 1
@@ -48,44 +57,54 @@ def move_files(line, last):
         first_index += 1
     return calculate_checksum(line)
 
-
 def move_files_that_fit(list_of_batches):
+    # Start at the end of the list and go backwards. If we find a file start from the left side and see if there is a space where it fits
     index = len(list_of_batches) - 1
     while index > 0:
         if list_of_batches[index][0] == ".":
             index -= 1
             continue
+        # File found, start search from left side
         for left in range(index):
             if not list_of_batches[left][0] == ".":
                 continue
+            # If their size is equal swap them
             if list_of_batches[left][1] == list_of_batches[index][1]:
                 list_of_batches[left] = list_of_batches[index]
                 new_batch = (".",list_of_batches[index][1])
                 list_of_batches[index] = new_batch
                 break
+            # If there is more free space than the size of the file
             elif list_of_batches[left][1] > list_of_batches[index][1]:
-                new_batch = (list_of_batches[left][0], list_of_batches[left][1] - list_of_batches[index][1])
+                # Create a new batch of empty space based on how much is left after moving
+                new_batch = (".", list_of_batches[left][1] - list_of_batches[index][1])
+                # We move the file and add the remaining space
                 list_of_batches[left:left+1] = list_of_batches[index], new_batch
                 index += 1
+                # If the next batch is empty space we merge
+                if list_of_batches[left + 2][0] == ".":
+                    list_of_batches[left + 1] = (".", list_of_batches[left+1][1] + list_of_batches[left + 2][1])
+                    list_of_batches.pop(left + 2)
+                    index -= 1
+                
                 new_batch = (".",list_of_batches[index][1])
                 list_of_batches[index] = new_batch
+                # Check if there is empty space before and after where the file was, then merge
+                if (list_of_batches[index - 1][0] == "."):
+                    list_of_batches[index - 1] = (".", list_of_batches[index - 1][1] + list_of_batches[index][1])
+                    list_of_batches.pop(index)
+                if (list_of_batches[index + 1][0] == "."):
+                    list_of_batches[index] = (".", list_of_batches[index][1] + list_of_batches[index + 1][1])
+                    list_of_batches.pop(index + 1)
+
                 break
         index -= 1
-        i = 1
-        while i < len(list_of_batches):
-            if list_of_batches[i][0] == list_of_batches[i-1][0]:
-                list_of_batches[i-1] = (list_of_batches[i-1][0],list_of_batches[i-1][1] + list_of_batches[i][1])
-                list_of_batches.pop(i)
-                i -= 1
-            i += 1
-        
+    # Rebuild the row from the batches, one character per element
     rebuilt_files = []
     for i in list_of_batches:
         for j in range(i[1]):
             rebuilt_files.append(i[0])
     return calculate_checksum(rebuilt_files)
-    
-    
 
 line, last, list_of_batches = read_file()
 print("Part 1:",move_files(line, last))
